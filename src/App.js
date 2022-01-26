84c4dd35480ac29c7428a6866b014a5cea2319f0
Serkan TOGAL <serkan.togal@altay.com.tr>
2022-01-26 17:03:33 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
+/*
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          İlk React Projesi
+          function yöntemi ile çıktı elde edelim.
         </p>
       </header>
     </div>
   );
+}
+*/
+
+class App extends Component
+{
+  render(){
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+            Component yöntemi ile çıktı elde edelim.
+            </p>
+          </header>
+        </div>
+    );
+  }
 }
 
 export default App;
