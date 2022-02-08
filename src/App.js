f176b82cb209329ca70beda7c6aa85d8995e6d4a
Serkan TOGAL <serkan.togal@altay.com.tr>
2022-02-08 15:30:28 +0300

    master

--- src/App.js
+++ src/App.js
@@ -20,12 +20,19 @@ function App() {
 class App extends Component
 {
   render(){
+    const yas = 36;
     return (
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <p>
             Component yöntemi ile çıktı elde edelim.
+            <br />
+            <br />
+            Benim yaşım {yas}. Bu sayıya göre 
+            {
+              yas > 35 ? " yaşlıyım" : " gencim"
+            }
             </p>
           </header>
         </div>
