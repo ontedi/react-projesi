9fe5fcc1156410464a0d559a16c3f5bd61dc36cb
Serkan TOGAL <serkan.togal@altay.com.tr>
2022-02-08 16:59:30 +0300

    master

--- src/ders1-css-islemleri.js
+++ src/ders1-css-islemleri.js
@@ -15,6 +15,13 @@ class Ders1CssIslemleri extends Component {
         <div className='kutu3'>
           Ders 1 CSS İşlemleri Kutu 3
         </div>
+        <div style={{color:"#000", fontSize:"23px", marginTop:"10px", backgroundColor:"rgba(199, 145, 63, 0.644)", padding:"6px"}}>
+          Ders 1 CSS İşlemleri Kutu 4
+        </div>
+        <div className='clearfix'></div>
+        <div style={{fontSize:"50px"}} className="kutu5">
+          Ders 1 CSS İşlemleri Kutu 5
+        </div>
     </div>
     );
   }
