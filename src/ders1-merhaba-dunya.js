0edb6ca2bb10b6a511e04a753c17a8b391679d6c
Serkan TOGAL <serkan.togal@altay.com.tr>
2022-02-09 17:57:58 +0300

    master

--- src/ders1-merhaba-dunya.js
+++ src/ders1-merhaba-dunya.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react'
 class Ders1MerhabaDunya extends Component {
   render() {
     return (
-      <div>ders1-merhaba-dunya</div>
+      <div>
+        <h3>Ders 1</h3>
+        Ekrana "Merhaba Dünya" Yazalım
+      </div>
     )
   }
 }
-
 export default Ders1MerhabaDunya;
