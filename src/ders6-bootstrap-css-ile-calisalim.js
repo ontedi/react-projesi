7da5e3f2458e539f13c5207b23f6b33a697cabf6
Serkan TOGAL <ontedi@outlook.com>
2022-04-16 18:20:31 +0300

    master

---
+++ src/ders6-bootstrap-css-ile-calisalim.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Ders6BootstrapCssileCalisalim extends Component {
+  render() {
+    return (
+      <div>ders6-bootstrap-css-ile-calisalim</div>
+    )
+  }
+}
