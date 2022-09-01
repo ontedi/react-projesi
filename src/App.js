0faad7a2a30ab5c6f385ef32249525256caacb53
Serkan TOGAL <serkan.togal@altay.com.tr>
2022-09-01 10:19:08 +0300

    - Yorumlar örneği yapıldı. - Yorum listeleme, değer gönderme, css işlemleri vs..

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import React  from 'react';
-import Z3StateLess from './z3-StateLess';
+import Z4Yorumlar from './z4-Yorumlar';
+
 
 function App() {
   return (
     <div className="container">
       <h1>App.js İçerisi</h1>
-
-      <Z3StateLess />
+      <div className='yorumlar-kapsul'>
+        <Z4Yorumlar isim="Serkan" mesaj="react ile ilgili bu yorum harika" gun="3" avatar='https://www.cizgi.site/_d/avatar/5.png' />
+        <Z4Yorumlar isim="Ufuk" mesaj="Nativescript hepsini yener" gun="2" avatar='https://www.cizgi.site/_d/avatar/1.png' />
+        <Z4Yorumlar isim="Cagan" mesaj="Tabela işinde iyi para var." gun="44" avatar='https://www.cizgi.site/_d/avatar/2.png' />
+      </div>
     </div>
   );
 }
---
+++ src/z4-Yorumlar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+ const z4Yorumlar = ({avatar, isim, mesaj, gun}) => {
+  return (
+    <div className='yorum d-flex'>
+        <div className='sol col-md-2'>
+            <div className='isim'>{isim}</div>
+            <div className='avatar'><img src={avatar} /></div>
+            <div className='tarih'>{gun} gün önce</div>
+        </div>
+        <div className='sag col-md-10'>{mesaj}</div>
+    </div>
+  )
+}
+
+export default  z4Yorumlar;
